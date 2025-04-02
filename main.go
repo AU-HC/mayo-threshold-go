e2fe9b443fc23de998b196b96f24eed4fd9dfe26
AU-HC <202004086@post.au.dk>
2025-04-02 12:42:40 +0200

    adds comments

--- main.go
+++ main.go
@@ -39,7 +39,7 @@ func main() {
 	// Step 5 of solve
 	mpc.ComputeAInverse(parties)
 	// Steps 6-9 of solve
-	mpc.ComputeLittleX(parties) // TODO: figure out if the spec is correct
+	mpc.ComputeLittleX(parties) // TODO: figure out if the spec is correct / test
 	// ** Algorithm solve **
 
 	// Step 7-8 of sign
